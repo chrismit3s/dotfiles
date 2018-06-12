dc44675cbc42246093a9958171fcb5784b7fbc44
chrismit3s <chrissidtmeier@gmail.com>
2018-06-12 20:02:28 +0200

    Changed symlink script

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-# for every file in the directory...
-for filename in ~/dotfiles/*
-do	# thats not the README.md, this scrips or whos filename is already taken
-	if [ $filename != ~/dotfiles/create-symlinks.sh ] && \
-	   [ $filename != ~/dotfiles/README.md ] && \
-	   [ ! -e ~/.$(basename $filename) ]
-	then
-		# create a hidden symlink to it
-		ln -s $filename ~/.$(basename $filename)
-	fi
+files="bashrc gdbinit gitconfig vim vimrc weechat zsh zshrc"
+
+# for every file in files
+for filename in $files
+do
+	# create a hidden symlink to it
+	ln -s $PWD/$filename $HOME/.$filename
 done

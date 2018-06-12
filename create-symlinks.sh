9bdc268479a93e84b837a8879f8a45f853b619e6
chrismit3s <chrissidtmeier@gmail.com>
2018-06-12 20:22:49 +0200

    Fixed symlink script

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 files="bashrc gdbinit gitconfig vim vimrc weechat zsh zshrc"
+source=~/dotfiles
+dest=~
 
 # for every file in files
 for filename in $files
 do
 	# create a hidden symlink to it
-	ln -s $PWD/$filename $HOME/.$filename
+	ln -s $source/$filename $dest/.$filename
 done
